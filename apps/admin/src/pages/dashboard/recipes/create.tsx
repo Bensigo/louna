df49766915f2caaababda4166173da57e2e3111a
bensigo <sigocode@gmail.com>
2024-04-01 22:44:24 +0400

    fix: build

--- apps/admin/src/pages/dashboard/recipes/create.tsx
+++ apps/admin/src/pages/dashboard/recipes/create.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AuthGuard } from '~/shared/AuthGuard'
 import AppLayout from '~/shared/DashboardNav'
-import { CreateRecipesWrapper } from '~/ui/recipes/CreateRecipeWraper'
+import { CreateRecipesWrapper } from '../../../ui/recipes/CreateRecipeWraper'
 
 
 
