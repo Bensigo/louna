de38b11d6b80984caa5c9a49a47329248f798ce8
bensigo <sigocode@gmail.com>
2024-04-02 13:34:22 +0400

    fix: builds

--- apps/admin/src/utils/getFileurl.ts
+++ apps/admin/src/utils/getFileurl.ts
@@ -1,14 +1,14 @@
 
 import { env } from "../env.mjs";
 
-
+const IMAGE_URL = env.IMG_SERVER_URL  || 'https://solu-file-bucket.vercel.app/' 
 export function buildFileUrl(repo: string, key: string) {
-    const BASE_URL =  env.NEXT_PUBLIC_IMG_SERVER_URL  ||  'http://localhost:6060' 
+    const BASE_URL =  IMAGE_URL  ||  'http://localhost:6060' 
     return `${BASE_URL}/api/${repo}/${key}`
 }
 
 
 export function buildFileUrlV2(key: string) {
-    const BASE_URL =  env.NEXT_PUBLIC_IMG_SERVER_URL  ||  'http://localhost:6060' 
+    const BASE_URL =  IMAGE_URL  ||  'http://localhost:6060' 
     return `${BASE_URL}/api/${key}`
 }
